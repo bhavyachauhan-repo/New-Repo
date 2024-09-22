d7a6593191ed3ede9d3afda7d72270f5a6e5cdac
Bhavya Chauhan <bhavyachauhan220102@gmail.com>
2024-09-22 23:49:22 +0530

    22-9-2024

---
+++ Lecture05/reverceNo.java
@@ -0,0 +1,12 @@
+package Lecture05;
+
+public class reverceNo {
+    static void program(int n,int k){
+        for(int i=n;i>=k;i--)
+        System.out.print(i);
+    }
+    public static void main(String[] args) {
+        program(1234566, 0);
+    }
+    
+}
---
+++ array/displayArray.java
@@ -0,0 +1,22 @@
+package array;
+
+public class displayArray {
+    static void show(int arr[]){
+
+        for(int i=0;i<=arr.length;i++){
+            System.out.println(i);
+
+
+    }
+}
+    public static void main(String[] args) {
+        int[] arr={1,2,3,4};
+        // int[0]=10;
+        // int[1]=20;
+        // int[2]=30;
+        // int[3]=40;
+        // int[4]=50;
+        show(arr);
+        }
+   
+}
